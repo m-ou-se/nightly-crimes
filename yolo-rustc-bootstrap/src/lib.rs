c1dd5dfe089c8f0ca55f25e5f214f51d63d7d046
Mara Bos <m-ou.se@m-ou.se>
2021-07-03 13:53:40 +0200

    Remove unnecessary 'extern crate'.

--- yolo-rustc-bootstrap/src/lib.rs
+++ yolo-rustc-bootstrap/src/lib.rs
@@ -1,5 +1,3 @@
-extern crate proc_macro;
-
 #[proc_macro]
 pub fn do_crimes(_: proc_macro::TokenStream) -> proc_macro::TokenStream {
     if !std::env::args_os().any(|arg| arg == "--cfg=yolo_rustc_bootstrap") {
